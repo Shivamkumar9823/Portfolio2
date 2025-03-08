15b9a1eae1437dc8a105f4bb0b00caf729554c95
shivam kumar <shivamkumarsingh.sk.2020@gmail.com>
2025-03-08 22:41:23 +0530

    fixing contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -66,7 +66,7 @@ const handleSubmit = (e) => {
               <i className="fas fa-envelope"></i>
               <div>
                 <h3>Email</h3>
-                <p>shivamkumarsingh.sk.2020@gmail.com</p>
+                <p>shivamkumarsingh2020@gmail.com</p>
               </div>
             </div>
             
